7993dd2704cc289ea48d70ccd281cae6bc6eaf92
SwarnJyoti <swarnjyotikeshri@gmail.com>
2025-04-24 21:04:39 +0530

    Update API URL to use env variable in api.ts

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000';
+//const API_URL = 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 export const getCustomers = async () => {
   const response = await axios.get(`${API_URL}/customers`);
